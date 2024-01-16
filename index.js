c310b045935c4467980adc938346a15f512645ab
Ayaka <ayamail2000@gmail.com>
2024-01-16 17:51:02 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// index.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Your JavaScript code goes here
+
+    // "Hello, world"を表示する
+    document.body.textContent = 'Hello, Vanilla!';
+});
