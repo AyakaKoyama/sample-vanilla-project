b7b6a27acde62abc308d742dcd3839007c34d158
Ayaka <ayamail2000@gmail.com>
2024-01-19 11:56:03 +0900

    push

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // index.js
 document.addEventListener('DOMContentLoaded', function () {
-    // Your JavaScript code goes here
 
     // "Hello, world"を表示する
     document.body.textContent = 'Hello, Vanilla!';
